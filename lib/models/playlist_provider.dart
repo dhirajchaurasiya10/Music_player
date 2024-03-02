188be75408d38ee69948c1dd705ff7b7c2cb6b29
dhirajchaurasiya10 <starkjon224@gmail.com>
2024-03-02 23:23:28 +0545

    updated

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:music_player/models/song.dart';
 
@@ -25,15 +26,112 @@ class PlaylistProvider extends ChangeNotifier {
   int? _currentSongIndex;
 
 
+  //audio player
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+
+  //durations
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+
+  //constructors
+  PlaylistProvider(){
+    listenToDuration();
+    }
+
+//initially not playing
+bool _isplaying =false;
+
+void play() async{
+  final String path = _playlist[_currentSongIndex!].audioPath;
+  await _audioPlayer.stop();
+  await _audioPlayer.play(AssetSource(path));
+  _isplaying= true;
+  notifyListeners();
+}
+
+void pause() async{
+  await _audioPlayer.pause();
+  _isplaying=false;
+  notifyListeners();
+}
+
+void resume() async {
+  await _audioPlayer.resume();
+  _isplaying=true;
+  notifyListeners();
+}
+
+void pauseOrResume() async {
+  if(_isplaying) {
+    pause();
+  } else {
+     resume();
+  }
+  notifyListeners();
+}
+
+void seek(Duration position) async {
+  await _audioPlayer.seek(position);
+}
+
+void playNextSong() {
+  if(_currentSongIndex != null){
+    if(_currentSongIndex! < _playlist.length-1){
+      currentSongIndex = _currentSongIndex! + 1;
+    }else {
+      currentSongIndex = 0;
+    }
+  }
+}
+
+void playpreviousSong() async {
+  if(_currentDuration.inSeconds >2) {}
+  else {
+    if(_currentSongIndex! >0){
+      currentSongIndex = _currentSongIndex! -1;
+    }
+    else {
+      currentSongIndex = _playlist.length-1;
+    }
+  }
+}
+
+
+
+
+
+  void listenToDuration() {
+//listen for total duration
+_audioPlayer.onDurationChanged.listen((newDuration) {
+  _totalDuration = newDuration;
+  notifyListeners();
+});
+
+//for current duration 
+_audioPlayer.onPositionChanged.listen((newPosition) {
+  _currentDuration=newPosition;
+});
+
+//for song completion
+_audioPlayer.onPlayerComplete.listen((event) {playNextSong();});
+
+}
 
   List<Song> get playlist => _playlist;
   int? get currentSongIndex => _currentSongIndex;
+  bool get isPlaying => _isplaying;
+  Duration get currentDuration=> _currentDuration;
+  Duration get totalDuration=>_totalDuration;
 
 
   set currentSongIndex(int? newIndex){
 
     _currentSongIndex = newIndex;
 
+    if(newIndex !=null){
+      play();
+    }
     notifyListeners();
   }
 }
